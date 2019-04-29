3bbf5e7d68b8b40ccedc664e4d5ec8765dcbcbd7
SvetlinNikolov <daxteranikolov@gmail.com>
2019-04-29 21:07:12 +0300

    Uploading Days - 5,6,7

---
+++ Day_6_Let's_Review/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Day_6_Let_s_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWordsToRead = int.Parse(Console.ReadLine());
+
+            string even = string.Empty;
+            string odd = string.Empty;
+
+            for (int i = 0; i < numberOfWordsToRead; i++)
+            {
+                string word = Console.ReadLine();
+
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (j%2==0)
+                    {
+                        even += word[j];
+                    }
+                    else
+                    {
+                        odd += word[j];
+                    }
+                }
+                Console.Write(even+" ");
+                Console.WriteLine(odd);
+
+                even = string.Empty;
+                odd = string.Empty;
+            }
+        }
+
+    }
+}
---
+++ Day_7_Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day_7_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
+    }
+}
